aba26371f5ec84cbe90bbaa4fa49c74f710ee2f4
MaxLepan <max.lepan@laposte.net>
2023-06-10 23:34:12 +0200

    ADD [Marker View] Added species get when clicked on a marker

--- lib/Marker/marker_view.dart
+++ lib/Marker/marker_view.dart
@@ -36,23 +36,35 @@ class MarkerView extends StatelessWidget {
         markerIcon = CustomIcons.spider;
     }
 
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            height: 500,
-            child: ExtendedImage.network(
-              marker.image,
-              fit: BoxFit.cover,
+    return FutureBuilder<Species>(
+      future: SpeciesViewModel().getSpeciesByName(marker.speciesName),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return const Text(
+              'Une erreur s\'est produite lors du chargement des données.');
+        } else {
+          Species species = snapshot.data!;
+
+          return Container(
+            child: Column(
+              children: [
+                Container(
+                  height: 500,
+                  child: ExtendedImage.network(
+                    marker.image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Icon(markerIcon),
+                Text(marker.speciesName, style: textStyle),
+                Text('La découverte de ${marker.userId}'),
+              ],
             ),
-
-          ),
-          Icon(markerIcon),
-          Text(marker.speciesName,style: textStyle),
-          Text('La découverte de ${marker.userId}'),
-
-        ],
-      ),
+          );
+        }
+      },
     );
   }
 }
