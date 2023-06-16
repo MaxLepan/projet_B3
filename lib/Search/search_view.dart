4a004546930c8f33299384f09f6b5e69497e3e78
Max <max.lepan@laposte.net>
2023-06-16 20:10:58 +0200

    ADD [Basic search] Added basic search style

--- lib/Search/search_view.dart
+++ lib/Search/search_view.dart
@@ -1,3 +1,4 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:projet_b3/Search/search_view_block_question_card.dart';
@@ -60,20 +61,19 @@ class SearchViewState extends State<SearchView> {
               padding: const EdgeInsets.only(left: 28, right: 28, bottom: 20),
               child: TextField(
                 decoration: const InputDecoration(
-                  hintText: "Trouver une bête",
-                  prefixIcon:
-                  IconButton(onPressed: null, icon: Icon(Icons.search)),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                    borderSide: BorderSide(color: greenBrown),
-                  )
-                ),
+                    hintText: "Trouver une bête",
+                    prefixIcon:
+                        IconButton(onPressed: null, icon: Icon(Icons.search)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      borderSide: BorderSide(color: greenBrown),
+                    )),
                 onChanged: (text) async {
                   List<Species> speciesList =
-                  await speciesViewModel.getSpeciesStartingBy(text);
+                      await speciesViewModel.getSpeciesStartingBy(text);
                   setState(() {
                     this.speciesList = speciesList;
                   });
@@ -92,9 +92,10 @@ class SearchViewState extends State<SearchView> {
                 child: Column(
                   children: const [
                     QuestionCard(
-                        question: "Quelle est cette petite bête ?",
-                        imagePath: "assets/images/search_view.png",
-                        route: "/transition",),
+                      question: "Quelle est cette petite bête ?",
+                      imagePath: "assets/images/search_view.png",
+                      route: "/transition",
+                    ),
                     SizedBox(height: 20),
                     QuestionCard(
                         question: "Quelles espèces observer dans le coin ?",
@@ -111,21 +112,75 @@ class SearchViewState extends State<SearchView> {
                 physics: const NeverScrollableScrollPhysics(),
                 itemCount: speciesList.length,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 4.0,
-                  mainAxisSpacing: 4.0,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,
+                  childAspectRatio: 0.95,
                 ),
+                padding: const EdgeInsets.symmetric(horizontal: 28),
                 itemBuilder: (BuildContext context, int index) {
+                  Species species = speciesList[index];
+                  Color borderColor;
+
+                  if (species.category.toLowerCase() == "reptile") {
+                    borderColor = purple_02;
+                  } else if (species.category.toLowerCase() == "amphibien") {
+                    borderColor = mint_02;
+                  } else if (species.category.toLowerCase() == "insecte") {
+                    borderColor = orange_02;
+                  } else {
+                    borderColor = strawberry_02;
+                  }
+
                   return GestureDetector(
                     onTap: () {
-                      Navigator.pushReplacementNamed(
-                          context, '/sheet',
-                          arguments: speciesList[index]);
+                      Navigator.pushReplacementNamed(context, '/sheet',
+                          arguments: species);
                     },
-                    child: Image.network(
-                      speciesList[index].imageUrl ??
-                          "https://www.feteduviolon.com/wp-content/uploads/2023/02/placeholder-1.png",
-                      fit: BoxFit.cover,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: borderColor,
+                          width: 4,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                        color: darkBeige,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ExtendedImage.network(
+                            species.imageUrl!,
+                            height: 120,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                            cache: true,
+                            loadStateChanged: (ExtendedImageState state) {
+                              switch (state.extendedImageLoadState) {
+                                case LoadState.loading:
+                                  return Container(
+                                    color: Colors.grey,
+                                  );
+                                case LoadState.completed:
+                                  return null;
+                                case LoadState.failed:
+                                  return Container(
+                                    color: Colors.redAccent,
+                                  );
+                              }
+                            },
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 3),
+                            child: Text(
+                              "${species.name[0].toUpperCase()}${species.name.substring(1).toLowerCase()}",
+                              style: smallTitle,
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 },
